8cae2b1eb3d255e1a600f2d72470209eaebbb1bb
DYT-Flyer <thomas.sherk1@gmail.com>
2026-07-27 17:18:54 -0400

    attempted to validate security certificate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client"],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'deepputs.com',
+          },
+        ],
+        destination: 'https://www.deepputs.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
